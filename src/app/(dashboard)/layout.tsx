6dd9600c5c8dbf2fccdc5a12037d2c33740fe049
Timo Heman <timoheman16@gmail.com>
2024-12-05 17:29:56 +0100

    added menu elements

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import Menu from "@/components/menu";
+
 export default function DashBoardLayout({
   children,
 }: Readonly<{
@@ -6,12 +11,19 @@ export default function DashBoardLayout({
   return (
     <main className="h-screen flex">
       {/* SIBEBAR */}
-      <aside className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-blue-100">
-        Side Bar
+      <aside className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SchoolMNG</span>
+        </Link>
+        <Menu />
       </aside>
 
       {/* MAIN CONTENT */}
-      <section className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-green-100">
+      <section className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">
         Main Cotent
       </section>
     </main>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +115,34 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((menuitem) => (
+        <div className="flex flex-col gap-2" key={menuitem.title}>
+          <h6 className="hidden lg:block text-gray-400 font-light my-4">
+            {menuitem.title}
+          </h6>
+          {menuitem.items.map((itemlink) => (
+            <Link
+              href={itemlink.href}
+              key={itemlink.label}
+              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+            >
+              <Image
+                src={itemlink.icon}
+                alt={`${itemlink.label} menu link`}
+                width={20}
+                height={20}
+              />
+              <span className="hidden lg:block">{itemlink.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
