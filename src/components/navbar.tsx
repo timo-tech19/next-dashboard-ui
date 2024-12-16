fe8fd5ca49b61f3213d32f849babdf308b1ab79a
Timo Heman <timoheman16@gmail.com>
2024-12-16 13:13:56 +0100

    added authentication and user roles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 const Navbar = () => {
@@ -33,13 +34,14 @@ const Navbar = () => {
           <span className="text-xs leading-3 font-medium">James Crawley</span>
           <span className="text-[10px] text-gray-500 text-right">Admin</span>
         </div>
-        <Image
+        {/* <Image
           src="/avatar.png"
           alt="User"
           width={36}
           height={36}
           className="rounded-full"
-        />
+        /> */}
+        <UserButton />
       </div>
     </nav>
   );
